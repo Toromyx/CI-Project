7c24b41039e125777561e178ba1a03097b7b94fb
Friederike <Friederike@DESKTOP-ND1NTI0>
2016-05-23 20:49:37 +0200

    New parser, took out all the functions we don't need but the arff ones, maybe they turn out to be useful afterall

--- FirstSteps/src/Parser.java
+++ FirstSteps/src/Parser.java
@@ -1,16 +1,16 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 
 import weka.classifiers.functions.MultilayerPerceptron;
 import weka.core.Instances;
-import weka.core.converters.ArffLoader.ArffReader;
-import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 import weka.core.converters.CSVSaver;
 
-//to read in file: run configurations > (x)Arguments: "project_training.txt""output.arff", press 'apply'
+
+/*
+ * Friederike Hanssen, Thomas Gauges
+ */
+//to read in file: run configurations > (x)Arguments: "project_training.txt", press 'apply'
 public class Parser {
 
 	private static Instances data;
@@ -30,15 +30,15 @@ public class Parser {
 		return data;
 	}
 	
-	private Instances loadARFF(String filename) throws IOException {
+	/*private Instances loadARFF(String filename) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(filename));
 		ArffReader loader = new ArffReader(reader);
 		data = loader.getData();
 		data.setRelationName("TrainingData");
 		return data;
-	}
+	}*/
 
-	// Arff is wekas prefered file system
+	/*// Arff is wekas prefered file system
 	private void createArff(String filename) throws IOException {
 		ArffSaver saver = new ArffSaver();
 		saver.setFile(new File(filename));
@@ -46,6 +46,7 @@ public class Parser {
 		saver.writeBatch();
 
 	}
+*/
 
 	// Creates a CSV output with tab separated values, as specified in output
 	// example
@@ -60,7 +61,7 @@ public class Parser {
 
 	public static void main(String[] args) throws IOException {
 		Parser parserCSV = null;
-		Parser parserArff = null;
+		
 		// maybe read in more then one file?
 		if (args.length < 1) {
 			System.err.println("You should specify at least one file as input!");
@@ -69,26 +70,24 @@ public class Parser {
 
 			parserCSV = new Parser();
 			parserCSV.loadCSV(args[0]);
-			//for fun
-			parserCSV.createArff("output.arff");
 			//required output
 			parserCSV.createCSV("output.txt");
-			
-			//arff parser, hoping to get the mulitlayer perceptron to work with those instances
-			parserArff = new Parser();
-			parserArff.loadARFF(args[1]);
-			
+						
 			try {
 				MultilayerPerceptron mlp = new MultilayerPerceptron();
+				System.out.println("Mlp initialized");
 				// Setting Parameters
 				mlp.setLearningRate(0.1);
 				mlp.setMomentum(0.2);
 				mlp.setTrainingTime(2000);
 				mlp.setHiddenLayers("3");
+				
+				//necessary, not sure why though
 				parserCSV.data.setClassIndex(data.numAttributes() - 1);
-				System.out.println("here");
+				
 				mlp.buildClassifier(parserCSV.data);
-				System.out.println("worked");
+				System.out.println("Classifier built");
+				
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
