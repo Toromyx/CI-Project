28398c7f4531306a425fb64531fc02e5f620fec0
Friederike <Friederike@DESKTOP-ND1NTI0>
2016-06-14 00:54:23 +0200

    Finished 9bit encoder and tested. changed encode single output. Shouldn't matter as it is only used internally in the encoder and not necessary for outside classes to be called

--- FirstSteps/src/encoder/NineBitEncoder.java
+++ FirstSteps/src/encoder/NineBitEncoder.java
@@ -1,68 +1,195 @@
 package src.encoder;
+/**
+ * @author Friederike
+ */
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.converters.CSVSaver;
 
-public class NineBitEncoder extends Encoder{
+public class NineBitEncoder extends Encoder {
 
-	private static int AAToBit(AminoAcid a){
-		
-		switch(a){
-			case A:
-				return 100001000;
-			case C:
-				return 000001001;
-			case D:
-				return 001001000;
-			case E:
-				return 001000100;
-			case F:
-				return 100100101;
-			case G:
-				return 000001000;
-			case H:
-				return 010100111;
-			case I:
-				return 100010011;
-			case K :
-				return 010000111;
-			case L :
-				return 100010001;
-			case M :
-				return 100000100;
-			case N:
-				return 000001000;
-			case P:
-				return 100000010;
-			case Q:
-				return 000000100;
-			case R:
-				return 010000110;
-			case S:
-				return 000001011;
-			case T:
-				return 000001010;
-			case V:
-				return 100011010;
-			case W:
-				return 100100110;
-			case Y:
-				return 000100111;
-			default:
-				System.err.println("Sequence could not be encoded correctly.");
-				return 111111111; //Amino acid X
+	public Instances encodeAll(String p) {
+
+		ArrayList<Attribute> x = new ArrayList();
+		AminoAcid[] seq = AAInterface.stringToAA(p);
+		x.add(new Attribute("first"));
+		x.add(new Attribute("second"));
+		x.add(new Attribute("third"));
+		x.add(new Attribute("fourth"));
+		x.add(new Attribute("fifth"));
+		x.add(new Attribute("sixth"));
+		x.add(new Attribute("seventh"));
+		x.add(new Attribute("eigth"));
+		x.add(new Attribute("ninth"));
+
+		Instances dataset = new Instances("Encoding", x, 9);
+
+		for (int i = 0; i < p.length(); i++) {
+			dataset.add(encodeSingle(seq[i]));
 		}
-	}
-	
-	public static void main(String[]args){
-		AminoAcid x = AminoAcid.A;
-		System.out.println(AAToBit(x));
-		
-	}
-	
-	@Override
-	public int[] encodeSingle(AminoAcid aa) {
-		// TODO Auto-generated method stub
-		return null;
+
+		/*
+		 * CSVSaver saver = new CSVSaver(); try { saver.setFile(new
+		 * File("encoding.txt")); saver.setInstances(dataset);
+		 * saver.setFieldSeparator("\t"); saver.writeBatch(); } catch
+		 * (IOException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 */
+
+		return dataset;
 	}
 
+	public Instance encodeSingle(AminoAcid aa) {
+
+		int[] attValues = new int[9];
+		Instance result = new DenseInstance(9);
+
+		switch (aa) {
+		case A:
+			int[] a = { 1, 0, 0, 0, 0, 1, 0, 0, 0 };
+			attValues = a;
+			break;
+		case C:
+			int[] c = { 0, 0, 0, 0, 0, 1, 0, 0, 1 };
+			attValues = c;
+			break;
+		case D:
+			int[] d = { 0, 0, 1, 0, 0, 1, 0, 0, 0 };
+			attValues = d;
+			break;
+		case E:
+			int[] encode = { 0, 0, 1, 0, 0, 0, 1, 0, 0 };
+			attValues = encode;
+			break;
+		case F:
+			int[] f = { 1, 0, 0, 1, 0, 0, 1, 0, 1 };
+			attValues = f;
+			break;
+		case G:
+			int[] g = { 0, 0, 0, 0, 0, 1, 0, 0, 0 };
+			attValues = g;
+			break;
+		case H:
+			int[] h = { 0, 1, 0, 1, 0, 0, 1, 1, 1 };
+			attValues = h;
+			break;
+		case I:
+			int[] i = { 1, 0, 0, 0, 1, 0, 0, 1, 1 };
+			attValues = i;
+			break;
+		case K:
+			int[] k = { 0, 1, 0, 0, 0, 0, 1, 1, 1 };
+			attValues = k;
+			break;
+		case L:
+			int[] l = { 1, 0, 0, 0, 1, 0, 0, 0, 1 };
+			attValues = l;
+			break;
+		case M:
+			int[] m = { 1, 0, 0, 0, 0, 0, 1, 0, 0 };
+			attValues = m;
+			break;
+		case N:
+			int[] n = { 0, 0, 0, 0, 0, 1, 0, 0, 0 };
+			attValues = n;
+			break;
+		case P:
+			int[] p = { 1, 0, 0, 0, 0, 0, 0, 1, 0 };
+			attValues = p;
+			break;
+		case Q:
+			int[] q = { 0, 0, 0, 0, 0, 0, 1, 0, 0 };
+			attValues = q;
+			break;
+		case R:
+			int[] r = { 0, 1, 0, 0, 0, 0, 1, 1, 0 };
+			attValues = r;
+			break;
+		case S:
+			int[] s = { 0, 0, 0, 0, 0, 1, 0, 1, 1 };
+			attValues = s;
+			break;
+		case T:
+			int[] t = { 0, 0, 0, 0, 0, 1, 0, 1, 0 };
+			attValues = t;
+			break;
+		case V:
+			int[] v = { 1, 0, 0, 0, 1, 1, 0, 1, 0 };
+			attValues = v;
+			break;
+		case W:
+			int[] w = { 1, 0, 0, 1, 0, 0, 1, 1, 0 };
+			attValues = w;
+			break;
+		case Y:
+			int[] y = { 0, 0, 0, 1, 0, 0, 1, 1, 1 };
+			attValues = y;
+			break;
+		default:
+			System.err.println("Amino acid was not found. Default value was used.");
+			int[] x = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+			attValues = x;
+
+		}
+		result.setValue(0, attValues[0]);
+		result.setValue(1, attValues[1]);
+		result.setValue(2, attValues[2]);
+		result.setValue(3, attValues[3]);
+		result.setValue(4, attValues[4]);
+		result.setValue(5, attValues[5]);
+		result.setValue(6, attValues[6]);
+		result.setValue(7, attValues[7]);
+		result.setValue(8, attValues[8]);
+
+		return result;
+	}
+
+	/*
+	 * public static void main(String[] args) {
+	 * 
+	 * NineBitEncoder test = new NineBitEncoder(); test.encodeAll("AAAAAAAAA");
+	 * 
+	 * AminoAcid a = AminoAcid.A; AminoAcid c = AminoAcid.C; AminoAcid d =
+	 * AminoAcid.D; AminoAcid e = AminoAcid.E; AminoAcid f = AminoAcid.F;
+	 * AminoAcid g = AminoAcid.G; AminoAcid h = AminoAcid.H; AminoAcid i =
+	 * AminoAcid.I; AminoAcid k = AminoAcid.K; AminoAcid l = AminoAcid.L;
+	 * AminoAcid m = AminoAcid.M; AminoAcid n = AminoAcid.N; AminoAcid p =
+	 * AminoAcid.P; AminoAcid q = AminoAcid.Q; AminoAcid r = AminoAcid.R;
+	 * AminoAcid s = AminoAcid.S; AminoAcid t = AminoAcid.T; AminoAcid v =
+	 * AminoAcid.V; AminoAcid w = AminoAcid.W; AminoAcid y = AminoAcid.Y;
+	 * AminoAcid x = AminoAcid.X;
+	 * 
+	 * 
+	 * System.out.println(test.encodeSingle(a));
+	 * System.out.println(test.encodeSingle(c));
+	 * System.out.println(test.encodeSingle(d));
+	 * System.out.println(test.encodeSingle(e));
+	 * System.out.println(test.encodeSingle(f));
+	 * System.out.println(test.encodeSingle(g));
+	 * System.out.println(test.encodeSingle(h));
+	 * System.out.println(test.encodeSingle(i));
+	 * System.out.println(test.encodeSingle(k));
+	 * System.out.println(test.encodeSingle(l));
+	 * System.out.println(test.encodeSingle(m));
+	 * System.out.println(test.encodeSingle(n));
+	 * System.out.println(test.encodeSingle(p));
+	 * System.out.println(test.encodeSingle(q));
+	 * System.out.println(test.encodeSingle(r));
+	 * System.out.println(test.encodeSingle(s));
+	 * System.out.println(test.encodeSingle(t));
+	 * System.out.println(test.encodeSingle(v));
+	 * System.out.println(test.encodeSingle(w));
+	 * System.out.println(test.encodeSingle(y));
+	 * System.out.println(test.encodeSingle(x));
+	 * System.out.println(test.encodeSingle(a));
+	 * 
+	 * 
+	 * }
+	 */
 }
