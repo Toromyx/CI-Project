1655fca6996dfa00c6e6dd5b9a5306c32812659c
Friederike <Friederike@DESKTOP-ND1NTI0>
2016-06-16 01:00:05 +0200

    Implemented a class for an ann. Since all methods will be similiar for all ann class, I decided on one, that just needs the the proper input model filename as parameter

--- FirstSteps/neuralNetworks/NeuralNetwork.java
+++ FirstSteps/neuralNetworks/NeuralNetwork.java
@@ -1,10 +1,66 @@
 package neuralNetworks;
-
+/**
+ * @author Friederike
+ */
 import weka.classifiers.functions.MultilayerPerceptron;
 import weka.core.Instances;
 
-public abstract class NeuralNetwork {
+public class NeuralNetwork {
+
+	private MultilayerPerceptron ann;
+
+	/**
+	 * Generates a new ANN. Needs the filename of the model. Methods to classify your data with a given model are then provided.
+	 * @param filename Proper filename such as Ic509bitAnn etc
+	 * @throws Exception something went wrong with reading in the file
+	 */
+	public NeuralNetwork(String filename) throws Exception {
+		ann = (MultilayerPerceptron) weka.core.SerializationHelper.read("filename");
+	}
+
+	/**
+	 * Classifies each instance of a dataset
+	 * @param testdata
+	 * @return classified testdata set
+	 */
+	public Instances applyData(Instances testdata) {
+		Instances output = new Instances(testdata, testdata.numInstances());
+		
+		for(int i = 0; i < testdata.numInstances(); i++){
+			double label = -1;
+			try {
+				label = ann.classifyInstance(testdata.instance(i));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			output.instance(i).setClassValue(label);
+		}
+		return output;		
+	}
 	
-	public abstract Instances run();
-	protected abstract void generateANN(Instances trainingsdata);
+	public void generateANN(Instances trainingsdata, String filename) {
+
+		MultilayerPerceptron ann = new MultilayerPerceptron();
+		ann.setLearningRate(0.1);
+		ann.setMomentum(0.2);
+		ann.setTrainingTime(2000);
+		ann.setHiddenLayers("2");
+
+		trainingsdata.setClassIndex(trainingsdata.numAttributes() - 1);
+
+		try {
+			ann.buildClassifier(trainingsdata);
+		} catch (Exception e) {
+			System.err.println("Classifier couldn't be build");
+			e.printStackTrace();
+		}
+
+		try {
+			weka.core.SerializationHelper.write(filename, ann);
+		} catch (Exception e) {
+			System.err.println("Classifier couldn't be stored");
+			e.printStackTrace();
+		}
+	}
 }
