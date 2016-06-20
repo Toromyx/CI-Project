bde6032e9342157d82e571b8370be256aa15483c
Friederike <Friederike@DESKTOP-ND1NTI0>
2016-06-21 00:29:37 +0200

    moved code for cross validation in own method to declutter main method

--- FirstSteps/src/defaultPackage/Main.java
+++ FirstSteps/src/defaultPackage/Main.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import neuralNetworks.NeuralNetwork;
 import parser.EncodeParser;
 import weka.core.Instances;
-
+/**
+ * 
+ * @author Friederike
+ *
+ */
 public class Main {
 
 
@@ -26,6 +30,7 @@ public class Main {
 			try {
 				//TODO???????
 				parser = new EncodeParser(){};
+				
 				//process input
 				predictData = initializeData(args[0], args[1]);
 				
@@ -38,7 +43,7 @@ public class Main {
 		}
 
 	}
-	// Instances predictData = parser.loadCSV(args[0]);
+
 
 	private static void printHelp() {
 		System.out.println("Please give the input in the following order: testdata encoding ann output-filename");
@@ -48,10 +53,11 @@ public class Main {
 	}
 
 	private static Instances initializeData(String predictData, String encoding) throws IOException {
-			Instances data = parser.
+//			Instances data = 
 			
-			return data;
+			return null;
 	}
+	
 	private static Instances applyANN(String encoding, String annVariable, Instances data) throws Exception{
 		String chosenAnn = chooseANN(encoding, annVariable);
 		NeuralNetwork ann = new NeuralNetwork(chosenAnn);
@@ -265,5 +271,51 @@ public class Main {
 		}
 		
 		return null;
+	}
+	
+	private static void validate(){
+		//to make Filters work (because of the "Cannot handle numeric class!"-Exception)
+	       NumericToNominal NtoN = new NumericToNominal();
+	       NtoN.setInputFormat(encodedData);
+	       encodedData = Filter.useFilter(encodedData, NtoN);
+	       
+	       //filter the Data to create 2 data-sets for the training and the cross-validation
+	       Resample res = new Resample();
+	   res.setNoReplacement(true);
+	   res.setSampleSizePercent(80);
+	   res.setInputFormat(encodedData);
+	   
+	   Instances train = Filter.useFilter(encodedData, res);
+	   
+	   res.setInvertSelection(true);
+	   res.setInputFormat(encodedData);
+	   Instances crossV = Filter.useFilter(encodedData, res);
+	       
+	       // create ann with train
+	       MultilayerPerceptron ann = new MultilayerPerceptron();
+	   ann.setLearningRate(0.1);
+	   ann.setMomentum(0.9);
+	   ann.setTrainingTime(10); // sonst dauert ewig 
+//			ann.setGUI(true);
+	   ann.setHiddenLayers("80,1");
+	   ann.buildClassifier(train);
+	   System.out.println();
+	   System.out.println("ANN is created");
+	   System.out.println();
+	   //System.out.println(ann.toString());
+	   
+	   System.out.println("Cross-Validatoin Output:");
+	   //create Evaluation for Cross-Validation
+	   Evaluation CV = new Evaluation(crossV);
+	   Random rand = new Random(1);
+	   CV.crossValidateModel(ann, crossV, 10, rand);
+	   System.out.println(CV.toSummaryString());
+	   
+	   //create Evaluation (now for train-set, just to make sure it works)
+	   Evaluation eval = new Evaluation(train);
+	   System.out.println();
+	   eval.evaluateModel(ann, train);
+	   System.out.println("Evaluation results (now for train-set, just to make sure it works):");
+	System.out.println(eval.toSummaryString());
 	}
 }
