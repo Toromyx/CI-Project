065e82537b5914368dec850cf62f35d90557122d
Daria <daria.elena.evseeva@gmail.com>
2016-06-23 22:54:35 +0200

    wrote some comments

--- FirstSteps/src/defaultPackage/Validation.java
+++ FirstSteps/src/defaultPackage/Validation.java
@@ -6,25 +6,11 @@ package defaultPackage;
  * 
  */
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 import java.util.Random;
-
-import com.sun.xml.internal.bind.v2.runtime.output.Encoded;
-
 import encoder.Encoder;
-import encoder.NineBitEncoder;
-import encoder.SixCharEncoder;
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.MultilayerPerceptron;
 import weka.core.Instances;
-import weka.filters.Filter;
-import weka.filters.supervised.instance.Resample;
-import weka.filters.unsupervised.attribute.NumericToNominal;
-import weka.core.converters.CSVLoader;
 
 /**
  * Class for Cross-Validation and Evaluation of the given ANN
@@ -37,14 +23,33 @@ public class Validation {
 	private MultilayerPerceptron ann;
 	private Encoder encoder;
 		
+	/**
+	 * The constructor create an object of the class Validation for a given data set and encoder. 
+	 * The data set will be encoded and saved as class field.
+	 * The empty ANN will be created.    
+	 * 
+	 * @param data : the training data set
+	 * @param encoder : chosen encoding type
+	 * @throws Exception
+	 */
+	
 	public Validation(Instances data, Encoder encoder) throws Exception{
 		
 		this.encodedData = encoder.encodeAll(data);
 		encodedData.setClassIndex(encodedData.numAttributes()-1);
-		
 		this.ann = new MultilayerPerceptron();
 		this.encoder=encoder;
 	}
+	
+	/**
+	 * Cross-Validation of the ANN with given network parameters to the encoded data set.
+	 * 
+	 * @param momentum
+	 * @param learningRate
+	 * @param learningSteps
+	 * @param HiddenLayers
+	 * @throws Exception
+	 */
 	
 	public void CrossValidate(double momentum, double learningRate, int learningSteps, String HiddenLayers)
 			throws Exception {
@@ -60,6 +65,11 @@ public class Validation {
 		
 		printResults(CV);
 	}
+	
+	/**
+	 * Help function for printing the cross validation results.
+	 * @param eval
+	 */
 	
 	private void printResults(Evaluation eval){
 		System.out.println("The given encoder is "+this.encoder.getClass());
